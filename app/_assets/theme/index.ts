96dfba7aa025379ef67eb536d07c87f0aa086577
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-14 15:41:42 +0330

    Add SampleColors

--- app/_assets/theme/index.ts
+++ app/_assets/theme/index.ts
@@ -1,6 +1,6 @@
 'use client'
 
-import { createTheme } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { Vazirmatn } from "next/font/google";
 
 const vazir = Vazirmatn({ subsets: ["latin"] });
@@ -8,5 +8,31 @@ const vazir = Vazirmatn({ subsets: ["latin"] });
 export const theme = createTheme({
     typography: {
         fontFamily: vazir.style.fontFamily
+    },
+    palette: {
+        body: {
+            dark: 'rgba(51, 51, 102, 1)',
+            main: 'sdfds'
+        },
+        appText: {
+            light: {
+                primary: 'rgba(153, 153, 179, 1)',
+
+            }
+        }
+        ,
     }
 })
+
+
+declare module '@mui/material/styles' {
+    interface Palette {
+        body: Palette['primary'],
+        appText: { light: Palette['primary'] }
+    }
+
+    interface PaletteOptions {
+        body?: PaletteOptions['primary'],
+        appText?: { light: { primary: string } };
+    }
+}
