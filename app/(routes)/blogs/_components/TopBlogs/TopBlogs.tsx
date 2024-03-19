80257dd4090bb966afd6781f8d56f20d4f0709d5
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-19 18:53:50 +0330

    init Container

--- app/(routes)/blogs/_components/TopBlogs/TopBlogs.tsx
+++ app/(routes)/blogs/_components/TopBlogs/TopBlogs.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const TopBlogs = () => {
   return (
-    <div>TopBlogs</div>
+    <div>
+      
+    </div>
   )
 }
---
+++ app/(routes)/blogs/_components/TopBlogs/components/Container.tsx
@@ -0,0 +1,6 @@
+import { Box, styled } from '@mui/material'
+import React from 'react'
+
+export const Container = styled(Box)(({theme})=>({
+    display:'flex',
+}))
