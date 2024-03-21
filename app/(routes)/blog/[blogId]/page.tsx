47265f086a7f53367199bc13e697946d70c22a73
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-21 23:28:36 +0330

    Init Blog Single Page

---
+++ app/(routes)/blog/[blogId]/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next'
+import React from 'react'
+
+export const metadata: Metadata = {
+    title: 'صفحه تک مقاله'
+}
+
+export default function page() {
+    return (
+        <div>صفحه تک مقاله</div>
+    )
+}
