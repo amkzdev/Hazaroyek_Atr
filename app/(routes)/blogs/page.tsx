d60b19de93119c940472393d6d4378f4bfd90c85
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-21 23:26:40 +0330

    Change Location Of Shortcut

--- app/(routes)/blogs/page.tsx
+++ app/(routes)/blogs/page.tsx
@@ -2,7 +2,8 @@ import { Box, Typography } from '@mui/material'
 import { Metadata } from 'next'
 import React from 'react'
 import { Breadcrumbs } from './_components/Breadcrumbs'
-import { Gift, PageContainer, Resourses, Shortcut, Title } from './_components'
+import { Gift, PageContainer, Resourses, Title } from './_components'
+import { Shortcut } from '@/_components'
 
 
 export const metadata: Metadata = {
@@ -14,9 +15,9 @@ export default function page() {
     <PageContainer>
       <Breadcrumbs />
       <Title />
-      <Resourses/>
-      <Shortcut/>
-      <Gift/>
+      <Resourses />
+      <Shortcut />
+      <Gift />
     </PageContainer>
   )
 }
