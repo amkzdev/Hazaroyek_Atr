e6550a39aed81a2a46438de13ae47ba7eddcc9c2
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-21 23:19:03 +0330

    Add Icon

---
+++ app/_assets/icons/ArrowDropLeft.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ArrowDropLeft = (props: React.SVGProps<SVGSVGElement>) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
+            <path d="M22 12C22 6.48 17.52 2 12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12ZM9.35 11.65L12.14 8.86C12.46 8.54 13 8.76 13 9.21V14.8C13 15.25 12.46 15.47 12.15 15.15L9.36 12.36C9.16 12.16 9.16 11.84 9.35 11.65Z" fill="currentColor" />
+        </svg>
+    )
+}
