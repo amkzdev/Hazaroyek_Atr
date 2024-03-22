37c693efc907bbc7571dc8bf7739b7896f211560
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-22 18:51:11 +0330

    Init Titles

--- app/(routes)/blog/[blogId]/page.tsx
+++ app/(routes)/blog/[blogId]/page.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import { PageContainer } from './components/PageContainer'
 import { Divider, Gift, Shortcut } from '@/_components'
 import { Author, Breadcrumbs } from './components'
-import { blogDetail} from './data.mock'
+import { blogDetail } from './data.mock'
+import { Grid } from '@mui/material'
 
 export const metadata: Metadata = {
     title: 'صفحه تک مقاله'
@@ -12,11 +13,24 @@ export const metadata: Metadata = {
 export default function page() {
     return (
         <PageContainer>
-            <Breadcrumbs/>
-            <Divider/>
-            <Author {...blogDetail.author}/>
-            <Gift/>
-            <Shortcut/>
+            <Breadcrumbs />
+            <Grid container spacing={4}>
+                <Grid xs={0} lg={2}>
+                    Related
+                </Grid>
+
+                <Grid xs={12} lg={8}>
+                    Content
+                </Grid>
+
+                <Grid xs={0} lg={2}>
+                    Titles
+                </Grid>
+            </Grid>
+            <Divider />
+            <Author {...blogDetail.author} />
+            <Gift />
+            <Shortcut />
         </PageContainer>
     )
 }
