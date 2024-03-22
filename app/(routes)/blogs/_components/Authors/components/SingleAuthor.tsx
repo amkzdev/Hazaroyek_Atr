47eb4303b3b6a8f9b638c5a374298bd8da73e974
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-22 17:51:01 +0330

    Modify Author

--- app/(routes)/blogs/_components/Authors/components/SingleAuthor.tsx
+++ app/(routes)/blogs/_components/Authors/components/SingleAuthor.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import React from 'react'
-import { BlogAuthorType, BlogSingleItem } from '@/_types'
-import { Avatar as MuiAvatar, Box, Typography, styled } from '@mui/material'
-import Image from 'next/image'
+
+import { BlogAuthorType } from '@types'
+import { Avatar as MuiAvatar, Box, Typography, styled, Link } from '@mui/material'
+import { Insta, Telegram } from '@icons'
 
 const Avatar = styled('img')(({ theme }) => ({
     borderRadius: '50%',
@@ -12,22 +13,60 @@ const Avatar = styled('img')(({ theme }) => ({
     objectFit: 'cover'
 })) as typeof MuiAvatar
 
+
+const ItemFrame = styled(Link)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    color: theme.palette.appText.light.secondary,
+    gap: theme.spacing(1),
+    alignItems: 'center',
+    fontSize: '16',
+    fontWeight: '500',
+    textDecoration: 'none'
+})) as typeof Link
+
+
+const Description = styled(Typography)(({ theme }) => ({
+    WebkitLineClamp: 5,
+    height: '7.5rem',
+    textAlign: 'center',
+    [theme.breakpoints.up('lg')]:{
+        width:'350px'
+    },
+    lineHeight: '1.5rem',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    display: "-webkit-box",
+    WebkitBoxOrient: 'vertical',
+    color:theme.palette.appText.light.primary
+})) as typeof Typography
+
+
 export const SingleAuthor = ({ fullname, description, articlesNumber, img, instagram, telegram }: BlogAuthorType) => {
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4, alignItems: 'center' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4.5, alignItems: 'center' }}>
 
             <Avatar src={img} />
 
             <Typography color={'appText.black'} fontSize={{ xs: 16, lg: 20 }} fontWeight='500'>{fullname}</Typography>
 
-            <Typography color={'appText.light.primary'} fontWeight={500} sx={{ WebkitLineClamp: 5, height: '7.5rem', textAlign: 'center', width: { lg: '350px' }, lineHeight: '1.5rem', overflow: 'hidden', textOverflow: 'ellipsis', display: "-webkit-box", WebkitBoxOrient: 'vertical' }} fontSize={{ xs: 14, lg: 16 }}>{description}{description}</Typography>
+            <Description  fontSize={{ xs: 14, lg: 16 }} fontWeight={500} >{description}</Description>
 
-            <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
-                <Box sx={{ display: 'flex', flexDirection: 'row' }}>تلگرام</Box>
-                <Box sx={{ display: 'flex', flexDirection: 'row' }}>اینستاگرام</Box>
+            <Box sx={{ display: 'flex', flexDirection: 'row', gap: 4, justifyContent: 'space-between' }}>
+                
+                <ItemFrame href={telegram}>
+                    <Telegram />
+                    <Typography>کانال تلگرام</Typography>
+                </ItemFrame>
+
+                <ItemFrame href={instagram}>
+                    <Insta />
+                    <Typography>کانال اینستاگرام</Typography>
+                </ItemFrame>
+
             </Box>
 
-            <Typography color={'appText.black'} fontSize={{ xs: 20, lg: 24 }} sx={{mt:3}} fontWeight={600} >{articlesNumber} مقاله ثبت شده</Typography>
+            <Typography color={'appText.black'} fontSize={{ xs: 20, lg: 24 }} sx={{ mt: 3 }} fontWeight={600} >{articlesNumber} مقاله ثبت شده</Typography>
         </Box>
     )
 }
