407c9f053556d74c1b3e9780015f31a216fe979f
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-24 18:19:48 +0330

    Add Header Button

--- app/_components/Header/components/HeaderContainer.tsx
+++ app/_components/Header/components/HeaderContainer.tsx
@@ -5,11 +5,15 @@ import { Box, styled } from "@mui/material";
 export const HeaderContainer = styled(Box)(({ theme }) => ({
     backgroundColor: theme.palette.body.dark,
     color:'white',
-    height: '100px',
+    // height: '100px',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    flexDirection:'column',
     // maxWidth:'1440px',
     margin: '0 auto',
-    fontSize:20
+    fontSize:20,
+    gap:4,
+    padding:'8px',
+    textDecoration:'none !important',
 })) as typeof Box
