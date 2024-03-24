02b5e34185c5f05196fe49def76d8cf79c2cfa67
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-24 18:20:45 +0330

    .

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
+
 import { HeaderContainer } from './components/HeaderContainer'
-import Link from 'next/link'
 import { CustomLink } from '../CustomLink'
 import { Box } from '@mui/material'
 
@@ -8,13 +8,10 @@ export const Header = () => {
   return (
     <HeaderContainer>
       هدر فروشگاه...
-      <Box sx={{display:'flex', flexDirection:'row', gap:2}}>
-
-      <CustomLink href={'/blogs'}>صفحه لیست بلاگ</CustomLink>
-      <CustomLink href={'/blog/sdfsd'}>صفحه تک بلاگ</CustomLink>
+      <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+        <CustomLink href={'/blogs'}>صفحه لیست بلاگ</CustomLink>
+        <CustomLink href={'/blog/sdfsd'}>صفحه تک بلاگ</CustomLink>
       </Box>
-      {/* <Link href={'/blogs'}>صفحه لیست بلاگ</Link>
-      <Link href={'/blog/sdfsd'}>صفحه تک بلاگ</Link> */}
     </HeaderContainer>
   )
 }
