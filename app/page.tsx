77fcbd7e504e435f0d1899cb5aebe34fe77785cd
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-03-24 18:24:01 +0330

    Add Redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import { Typography } from "@mui/material";
 import { Metadata } from "next";
-import { CenteredPage, CustomLink } from "./_components";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: 'صفحه اصلی | هزار و یک عطر'
@@ -8,9 +7,6 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <CenteredPage sx={{ gap: 2 }}>
-      <Typography>به فروشگاه اینترنتی هزار و یک عطر خوش آمدید.</Typography>
-      <CustomLink href="/blogs" >رفتن به صفحه لیست مقالات</CustomLink>
-    </CenteredPage>
+    redirect('/blogs')
   );
 }
